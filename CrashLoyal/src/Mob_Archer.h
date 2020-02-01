929813404e903819d7ad98bc2476db2098daa085
Kevin Dill <kdill4@gmail.com>
2020-02-01 17:55:07 -0500

    adjusted size

--- CrashLoyal/src/Mob_Archer.h
+++ CrashLoyal/src/Mob_Archer.h
@@ -7,7 +7,7 @@ class Mob_Archer : public Mob
 public:
 	virtual int GetMaxHealth() const { return 4; }
 	virtual float GetSpeed() const { return 0.01f; }
-	virtual float GetSize() const { return 3.f; }
+	virtual float GetSize() const { return 2.f; }
 	virtual int GetDamage() const { return 1; }
 	virtual float GetAttackTime() const { return 150; }
 };
